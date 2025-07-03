a65788ad82b56f200ea64644e30c6fda0eaea94f
shanawazpasha <shanawazpasha@gmail.com>
2025-07-03 12:57:53 -0500

    Add files via upload

---
+++ web_mvc_config.java
@@ -0,0 +1,21 @@
+package com.yourcompany.yourapp.config;
+
+import com.yourcompany.yourapp.interceptor.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/actuator/**", "/health", "/info", "/metrics");
+    }
+}
